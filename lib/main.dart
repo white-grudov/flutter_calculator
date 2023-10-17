fd8d8a66f3eb7c68bbaa8cd4a20532045ade3d40
whitegrudov <danekbel.1@gmail.com>
2023-10-17 18:18:24 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: Placeholder(),
+    );
+  }
+}
